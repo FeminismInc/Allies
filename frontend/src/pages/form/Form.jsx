b1fa42b7dca8d56034b1b505cdf128ab17921047
kennethhun <kenhun2020@gmail.com>
2024-09-25 20:19:22 -0700

    small update to form
    added values to form items

--- frontend/src/pages/form/Form.jsx
+++ frontend/src/pages/form/Form.jsx
@@ -48,20 +48,20 @@ export default function ProfileForm() {
             <form className="userInfo">
                 <div class="Username">
                     <label for="username">Username: </label>
-                    <input type="text" onChange={(e) => { setUsername(e.target.value) }} name="username" id="username" placeholder='Enter your username' required />
+                    <input type="text" value={username} onChange={(e) => { setUsername(e.target.value) }} name="username" id="username" placeholder='Enter your username' required />
                 </div>
                 <div class="Email">
                     <label for="email">Enter your email: </label>
-                    <input type="email" onChange={(e) => { setEmail(e.target.value) }} name="email" id="email" placeholder='Enter your email' required />
+                    <input type="email" value={email} onChange={(e) => { setEmail(e.target.value) }} name="email" id="email" placeholder='Enter your email' required />
                 </div>
                 <div class="Password">
                     <label for="password">Enter your password: </label>
-                    <input type="password" onChange={(e) => { setPassword(e.target.value) }} name="password" id="password" minlength="8" placeholder='Enter your password' required />
+                    <input type="password" value={password} onChange={(e) => { setPassword(e.target.value) }} name="password" id="password" minlength="8" placeholder='Enter your password' required />
                     <p className='passwordRec'>Use 8 or more characters with a mix of letters, numbers & symbols</p>
                 </div>
                 <div class="Handle">
                     <label for="handle">Enter your Handle (Name): </label>
-                    <input type="text" onChange={(e) => { setHandle(e.target.value) }} name="handle" id="handle" placeholder='Enter your profile name' required />
+                    <input type="text" value={handle} onChange={(e) => { setHandle(e.target.value) }} name="handle" id="handle" placeholder='Enter your profile name' required />
                 </div>
                 <div className="Pronouns">
                 <legend>Preferred Pronouns?</legend>
@@ -74,7 +74,7 @@ export default function ProfileForm() {
                 </div>
                 <div className="Birthdate">
                     <label for="birthdate">What is your birthday? </label>
-                    <input type="date" onChange={(e) => { setBirthdate(e.target.value) }} id="birthdate" name="birthdate"/>
+                    <input type="date" value={birthdate} onChange={(e) => { setBirthdate(e.target.value) }} id="birthdate" name="birthdate"/>
                 </div>
                 <p className='termsOfService'>By creating an account, you agree to the Terms of Use and Privacy Policy. </p>
                 <Button title = "Submit" onClick={submit} color="#bfa1f0"></Button>
