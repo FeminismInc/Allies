686daa8803664089b7948ffd745a34430a7d8c7b
abigailxcal <acalde1997@gmail.com>
2024-11-25 21:04:14 -0800

    wiped notifications button and fixed post page

--- frontend/src/pages/post/post.jsx
+++ frontend/src/pages/post/post.jsx
@@ -1,17 +1,34 @@
 import "./post.css";
-import {Button} from 'react-native';
+import { Button } from 'react-native';
 import { useNavigate } from 'react-router-dom';
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import axios from "axios"
 import { useState } from 'react';
 import Sidebar from '../../components/sidebar/Sidebar';
 
-export default function PostPage(){
-    
+export default function PostPage() {
+
     const uri = "http://localhost:5050/api";
     const navigate = useNavigate();
 
     const [text, setText] = useState('')
+    const [username, setUsername] = useState('');
+
+
+    useEffect(() => {
+        axios.get(`${uri}/users/findUser`, { withCredentials: true })
+            .then(response => {
+                
+                if (response.data.username) { //if a username was provided in the url, then we are trying to view their profile 
+                    setUsername(response.data.username)
+                }
+            })
+            .catch(error => {
+                console.error('Error fetching user:', error);
+            })
+    });
+
+
 
     const submit = async (e) => {
         console.log("entered submit");
@@ -22,27 +39,25 @@ export default function PostPage(){
             const post = await axios.post(`${uri}/posts/createPost`, { //create new following object
                 text,
             })
-
-            navigate('/profile')
-
+            navigate(`/profile/${username}`)
             // use $push to update a userDetail posts could send author ID rather than just author name and add it to user upon post creation
-            
+
         }
-        catch(e) {
+        catch (e) {
             console.log('Something went wrong creating a post')
         }
-        
+
     }
 
     return (
         <div className="postMainContent">
             <div className="sidebarContainer">
-                <Sidebar/>
+                <Sidebar />
             </div>
             <div className="postContainer">
                 <h1>What's on your mind? </h1>
                 <input type="text" value={text} onChange={(e) => { setText(e.target.value) }} name="postText" id="postText"></input>
-                <Button title = "Submit" onPress={submit} color="#bfa1f0" id="submitButton"/> 
+                <Button title="Submit" onPress={submit} color="#bfa1f0" id="submitButton" />
             </div>
         </div>
     )
